417a7ac402541b916597508b3e208f319664eabb
mahmoud-zahran021 <mahmoudzahran20025-arch@gmail.com>
2025-10-15 04:03:46 +0300

    version3

--- js/utils.js
+++ js/utils.js
@@ -541,3 +541,50 @@ export function cleanup() {
 
 console.log('✅ Utils module loaded (Secure & Optimized)');
 
+
+// Soft Ice Cream GSAP Animations - Isolated
+// Soft Ice Cream GSAP - Isolated Version
+(function() {
+  function animateIcecreamFrom(elem, direction) {
+    direction = direction || 1;
+    var x = 0,
+        y = direction * 100;
+    if(elem.classList.contains("icecream-gs-reveal-fromLeft")) {
+      x = -100;
+      y = 0;
+    } else if (elem.classList.contains("icecream-gs-reveal-fromRight")) {
+      x = 100;
+      y = 0;
+    }
+    elem.style.transform = "translate(" + x + "px, " + y + "px)";
+    elem.style.opacity = "0";
+    gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
+      duration: 1.25, 
+      x: 0,
+      y: 0, 
+      autoAlpha: 1, 
+      ease: "expo", 
+      overwrite: "auto"
+    });
+  }
+  
+  function hideIcecreamElem(elem) {
+    gsap.set(elem, {autoAlpha: 0});
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(ScrollTrigger);
+    
+    gsap.utils.toArray(".icecream-gs-reveal").forEach(function(elem) {
+      hideIcecreamElem(elem); // assure that the element is hidden when scrolled into view
+      
+      ScrollTrigger.create({
+        trigger: elem,
+        // markers: true,
+        onEnter: function() { animateIcecreamFrom(elem) }, 
+        onEnterBack: function() { animateIcecreamFrom(elem, -1) },
+        onLeave: function() { hideIcecreamElem(elem) } // assure that the element is hidden when scrolled into view
+      });
+    });
+  });
+})();
